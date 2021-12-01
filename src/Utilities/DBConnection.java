fbb8ba31d1ec29e7adb93852b6e93d7d3bfa49bd
samurainstein <eric.matelyan@gmail.com>
2021-12-01 12:33:07 -0500

    Initial setup fo DBConnection class

---
+++ src/Utilities/DBConnection.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Eric
+ */
+public class DBConnection {
+    
+    private static final String protocol = "jdbc";
+    private static final String vendorName = ":mysql:";
+    private static final String ipAddress = "127.0.0.1:3306";
+    private static final String dbName = "Local Host";
+    //Required for MySQL Connector/J driver 8.0.23 and above
+    private static final String timeConv = "?connectionTimeZone=SERVER";
+    private static final String jdbcUrl = protocol + vendorName + ipAddress + dbName + timeConv;
+    private static final String MYSQLJDBCDriver = "com.mysql.cj.jdbc.Driver";
+    private static Connection conn = null;
+    private static final String username = "root";
+    private static final String password = "My3ql!@#";
+    
+        /**
+     * This method starts the connection to the database. 
+     */
+    public static Connection startConnection() {
+        try {
+            Class.forName(MYSQLJDBCDriver);
+            conn = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connection Successful");
+        }
+        catch(ClassNotFoundException exception) {
+            //System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
+        catch(SQLException exception) {
+            //System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
+        
+        return conn;
+    }
+    
+    /**
+     * This method retrieves the connection to the database. 
+     */
+    public static Connection getConnection() {
+        return conn;
+    }
+    
+    /**
+     * This method closes the connection to the database. 
+     */
+    public static void closeConnection() {
+        try {
+            conn.close();
+            System.out.println("Connection Closed");
+        }
+        catch(SQLException exception) {
+            //do nothing
+        }
+    }
+    
+}
