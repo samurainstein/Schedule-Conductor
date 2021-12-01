111451b7dc72659ccc95ad651c8146a9057093d6
samurainstein <eric.matelyan@gmail.com>
2021-12-01 16:36:59 -0500

    Initial Setup of DBQuery class

---
+++ src/Utilities/DBQuery.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+/**
+ *This class is used to execute SQL statements on the database
+ * @author Eric
+ */
+public class DBQuery {
+    
+    private static PreparedStatement statement;
+    
+    /**
+     * This method is used to create a prepared statement object. 
+     * @param conn Connection object that is passed to the method
+     * @param sqlStatement SQL Statement that is prepared
+     * @throws SQLException
+     */
+    public static void setPreparedStatement(Connection conn, String sqlStatement) throws SQLException {
+        statement = conn.prepareStatement(sqlStatement);       
+    }
+    
+    /**
+     * This method is used to return a prepared statement object. 
+     * @return Returns a statement object
+     */
+    public static PreparedStatement getPreparedStatement() {
+        return statement;
+    }
+}
