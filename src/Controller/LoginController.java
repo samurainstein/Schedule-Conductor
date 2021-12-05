04890e9cdbc5b6b365a2f0245e3cb0febf6091fb
samurainstein <eric.matelyan@gmail.com>
2021-12-04 19:11:38 -0500

    Create Event Handlers for button click and keypress

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -16,6 +16,7 @@ import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -23,9 +24,12 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  * This class handles events on the Login screen
+ *
  * @author Eric Matelyan
  */
 public class LoginController implements Initializable {
@@ -35,16 +39,18 @@ public class LoginController implements Initializable {
     @FXML
     private TextField passwordTF;
     @FXML
+    private Button loginBT;
+    @FXML
     private Label zoneIDLabel;
-//    @FXML
-//    private Label welcomeLabel;
-//    @FXML
-//    private Label signInLabel;
-//    @FXML
-//    private Button loginButton;
 
     private String alertTitle = "Invalid username or password";
     private String alertText = "Username or password is incorrect";
+    
+
+    public LoginController() {
+
+    }
+
     /**
      * Initializes the controller class.
      */
@@ -52,36 +58,55 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         ZoneId zoneID = ZoneId.systemDefault();
         zoneIDLabel.setText("Location: " + zoneID.toString());
-        
-//        if(Locale.getDefault().getLanguage().equals("fr")) {
-//            welcomeLabel.setText("Bienvenue!");
-//            signInLabel.setText("Connectez-vous s'il vous plaît");
-//            usernameTF.setPromptText("Nom de l'utilisateur");
-//            passwordTF.setPromptText("Mot de passe");
-//            loginButton.setText("Se connecter");
-//            alertTitle = "Nom de l'utilisateur ou mot de passe invalide";
-//            alertText = "Nom de l’utilisateur ou mot de passe incorrect";
-//            zoneIDLabel.setText("Lieu: " + zoneID.toString());
-//        }
-          
-    }    
-
-    @FXML
-    private void onLogin(ActionEvent event) throws SQLException, IOException {
-        String username = usernameTF.getText();
-        String password = passwordTF.getText();
-        int id = InstrumentTeacherDAO.teacherLogin(username, password);
-        if(id == 0) {
-            Alerts.loginInvalid(alertTitle, alertText);
-//            ActivityLog.loginAttempt(userID, username);
-        }
-        
-        else {
-            System.out.println("Login Successful");
-//            ActivityLog.loginAttempt(userID, username);
-//            Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
-//            String pageTitle = PageLoader.getHomeTitle();
-//            PageLoader.pageLoad(event, root, pageTitle);  
-        }
+        usernameTF.setOnKeyPressed(keyPressHandler);
+        passwordTF.setOnKeyPressed(keyPressHandler);
+        loginBT.setOnKeyPressed(keyPressHandler);
+        loginBT.setOnAction(actionEventHandler);
     }
+
+//    @FXML
+//    private void onLogin(ActionEvent event) throws SQLException, IOException {
+//        String username = usernameTF.getText();
+//        String password = passwordTF.getText();
+//        int id = InstrumentTeacherDAO.teacherLogin(username, password);
+//        if (id == 0) {
+//            Alerts.loginInvalid(alertTitle, alertText);
+////            ActivityLog.loginAttempt(userID, username);
+//        } else {
+//            System.out.println("Login Successful");
+////            ActivityLog.loginAttempt(userID, username);
+////            Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
+////            String pageTitle = PageLoader.getHomeTitle();
+////            PageLoader.pageLoad(event, root, pageTitle);  
+//        }
+//    }
+    
+    EventHandler<KeyEvent> keyPressHandler = new EventHandler<KeyEvent>() {
+        public void handle(KeyEvent ke) {
+            if(ke.getCode() == KeyCode.ENTER ) {
+                String username = usernameTF.getText();
+                String password = passwordTF.getText();
+                int id = InstrumentTeacherDAO.teacherLogin(username, password);
+                if (id == 0) {
+                    Alerts.loginInvalid(alertTitle, alertText);
+                } else {
+                System.out.println("Login Successful");
+                }
+            }
+        }
+    };
+    
+    EventHandler<ActionEvent> actionEventHandler = new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent event) {
+            String username = usernameTF.getText();
+            String password = passwordTF.getText();
+            int id = InstrumentTeacherDAO.teacherLogin(username, password);
+            if (id == 0) {
+                    Alerts.loginInvalid(alertTitle, alertText);
+            } 
+            else {
+                System.out.println("Login Successful");
+            }
+        }             
+    };
 }
