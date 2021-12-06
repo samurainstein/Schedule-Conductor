8f777a09b8cee1ef8e459bbe905506015b8db319
samurainstein <eric.matelyan@gmail.com>
2021-12-06 16:38:08 -0500

    Initial Setup of appointmentDAO class

---
+++ src/DAO/AppointmentDAO.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.Appointment;
+import Model.Data;
+import Utilities.DBConnection;
+import Utilities.DBQuery;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Eric
+ */
+public class AppointmentDAO {
+    
+    /**
+     * Select statement for all rows in the appointments table. 
+     * 
+     */
+    public static void selectAppointments() throws SQLException {
+        try {
+            Data.clearAppointments();
+            Connection conn = DBConnection.getConnection();
+            String sqlStatement = "SELECT Appointment_ID, Title, Description, Location, Start, End, Student_ID, Teacher_ID "
+                                + "FROM appointments;";
+            DBQuery.setPreparedStatement(conn, sqlStatement);
+            PreparedStatement preparedStatement = DBQuery.getPreparedStatement();
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            while(resultSet.next()) {
+                int appointmentID = resultSet.getInt("Appointment_ID");
+                String title = resultSet.getString("Title");
+                String description = resultSet.getString("Description");
+                String location = resultSet.getString("Location");
+                Timestamp startTimestamp = resultSet.getTimestamp("Start");
+                LocalDateTime start = startTimestamp.toLocalDateTime();
+                Timestamp endTimestamp = resultSet.getTimestamp("End");
+                LocalDateTime end = endTimestamp.toLocalDateTime();
+                int studentId = resultSet.getInt("Student_ID");
+                int teacherId = resultSet.getInt("Teacher_ID");
+
+                Appointment appointment = new Appointment(appointmentID, title, description, 
+                            location, start, end, studentId, teacherId);
+
+                Data.addAppointment(appointment);
+            }
+        }
+        catch(SQLException exception) {
+                exception.printStackTrace();
+        }  
+    }
+}
