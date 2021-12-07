5de4474c61c91a03ec53482bc53105424623baae
samurainstein <eric.matelyan@gmail.com>
2021-12-07 18:55:45 -0500

    Add code to update appointment display on home page

--- src/Controller/HomeController.java
+++ src/Controller/HomeController.java
@@ -6,21 +6,18 @@
 package Controller;
 
 import DAO.AppointmentDAO;
-import DAO.InstrumentTeacherDAO;
 import Model.Appointment;
 import Model.Data;
-import Utilities.Alerts;
 import Utilities.PageLoader;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -46,6 +43,26 @@ public class HomeController implements Initializable {
     private Label logoutLabel;
     @FXML
     private Label notificationLBL;
+    @FXML
+    private Label studentLBL;
+    @FXML
+    private Label titleLBL;
+    @FXML
+    private Label locationLBL;
+    @FXML
+    private Label studentLBLtxt;
+    @FXML
+    private Label titleLBLtxt;
+    @FXML
+    private Label locationLBLtxt;
+    @FXML
+    private Label dateLBL;
+    @FXML
+    private Label timeLBL;
+    @FXML
+    private Label dateLBLtxt;
+    @FXML
+    private Label timeLBLtxt;
 
     /**
      * Initializes the controller class.
@@ -55,18 +72,53 @@ public class HomeController implements Initializable {
 //        logoutLabel = new Label();
 //        logoutLabel.setOnMouseClicked(logoutEventHandler); 
         loggedInTeacherId = Data.getLoggedInTeacherId();
+        Data.clearAppointments();
         try {
             AppointmentDAO.selectAppointments();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+        Data.clearTeacherAppointments();
         teacherAppointments = Data.getTeacherAppointments(loggedInTeacherId);
         if(teacherAppointments.size() > 0) {
+            notificationLBL.setText("Your next appointment");
+            studentLBL.setVisible(true);
+            titleLBL.setVisible(true);
+            locationLBL.setVisible(true);
+            studentLBLtxt.setVisible(true);
+            titleLBLtxt.setVisible(true);
+            locationLBLtxt.setVisible(true);
+            dateLBL.setVisible(true);
+            timeLBL.setVisible(true);
+            dateLBLtxt.setVisible(true);
+            timeLBLtxt.setVisible(true);
+            
             Appointment appointment = teacherAppointments.get(0);
-            notificationLBL.setText(appointment.getTitle());
+            //FIX THIS
+            int studentId = appointment.getStudentId();
+            String studentName = Data.getStudentName(studentId);
+            studentLBLtxt.setText(studentName);
+            titleLBLtxt.setText(appointment.getTitle());
+            locationLBLtxt.setText(appointment.getLocation());
+            LocalDateTime startDateTime = appointment.getStart();
+            LocalDate startDate = startDateTime.toLocalDate();
+            LocalTime startTime = startDateTime.toLocalTime();
+            dateLBLtxt.setText(startDate.toString());
+            timeLBLtxt.setText(startTime.toString());
+            
         }
         else {
             notificationLBL.setText("There are no upcoming appointments");
+            studentLBL.setVisible(false);
+            titleLBL.setVisible(false);
+            locationLBL.setVisible(false);
+            studentLBLtxt.setVisible(false);
+            titleLBLtxt.setVisible(false);
+            locationLBLtxt.setVisible(false);
+            dateLBL.setVisible(false);
+            timeLBL.setVisible(false);
+            dateLBLtxt.setVisible(false);
+            timeLBLtxt.setVisible(false);
         }
     }    
     
