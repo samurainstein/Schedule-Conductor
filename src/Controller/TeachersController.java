2bb89c651e81afd8104402cf1e5a3d7077b9920e
samurainstein <eric.matelyan@gmail.com>
2021-12-08 14:16:49 -0500

    Set event handlers for nav labels

--- src/Controller/TeachersController.java
+++ src/Controller/TeachersController.java
@@ -5,9 +5,13 @@
  */
 package Controller;
 
+import Utilities.EventHandle;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 
 /**
  * FXML Controller class
@@ -16,12 +20,41 @@ import javafx.fxml.Initializable;
  */
 public class TeachersController implements Initializable {
 
+    @FXML
+    private Label teachersLBL;
+    @FXML
+    private Label logoutLabel;
+    @FXML
+    private TableColumn<?, ?> idCol;
+    @FXML
+    private TableColumn<?, ?> name;
+    @FXML
+    private TableColumn<?, ?> country;
+    @FXML
+    private TableColumn<?, ?> division;
+    @FXML
+    private TableColumn<?, ?> postalCode;
+    @FXML
+    private TableColumn<?, ?> address;
+    @FXML
+    private TableColumn<?, ?> phone;
+    @FXML
+    private TableColumn<?, ?> instrument;
+    @FXML
+    private TableColumn<?, ?> availableOnline;
+    @FXML
+    private TableColumn<?, ?> availableInPerson;
+    @FXML
+    private Label homeLBL;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        homeLBL.setOnMouseClicked(EventHandle.navHomeEvent());
+        logoutLabel.setOnMouseClicked(EventHandle.navLogoutEvent());
+        teachersLBL.setOnMouseClicked(EventHandle.navTeachersEvent());
     }    
     
 }
