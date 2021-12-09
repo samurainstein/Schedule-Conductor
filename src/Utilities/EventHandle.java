2ed321a9ec1b66da93eed3cc355949a175094882
samurainstein <eric.matelyan@gmail.com>
2021-12-09 14:20:23 -0500

    Write code for Teachers Delete Event

--- src/Utilities/EventHandle.java
+++ src/Utilities/EventHandle.java
@@ -5,14 +5,23 @@
  */
 package Utilities;
 
+import DAO.AppointmentDAO;
 import DAO.InstrumentTeacherDAO;
 import Model.Data;
+import Model.InstrumentTeacher;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -132,6 +141,40 @@ public abstract class EventHandle {
             }
         };
         
+        return eventHandler;
+    }
+    
+    public static EventHandler<ActionEvent> teachersDeleteEvent(TableView<InstrumentTeacher> teachersTable) throws SQLException {
+        EventHandler<ActionEvent> eventHandler = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+
+                try {
+                    InstrumentTeacher teacher = teachersTable.getSelectionModel().getSelectedItem();
+                    int id = teacher.getId();
+                    try {
+                        AppointmentDAO.selectAppointments();
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                    boolean checkAssociated = Data.checkAssociatedAppointments(id);
+                    if (checkAssociated == true) {
+                        Alerts.associatedAppointment();
+                    } else {
+
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this customer?");
+                        Optional<ButtonType> result = alert.showAndWait();
+                        if (result.isPresent() && result.get() == ButtonType.OK) {
+                            InstrumentTeacherDAO.deleteTeacher(id);
+                            InstrumentTeacherDAO.selectTeachers();
+                            teachersTable.setItems(Data.getAllTeachers());
+                            Alerts.teacherDeleteConfirm();
+                        }
+                    }
+                } catch (NullPointerException exception) {
+                    Alerts.teacherDeleteNull();
+                }
+            }
+        };
         return eventHandler;
     }
 }
