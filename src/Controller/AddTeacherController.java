668bb79b4530ab81069e5a04fd8c4aeaa03572ee
samurainstein <eric.matelyan@gmail.com>
2021-12-12 12:59:09 -0500

    Set Event Handler for Cancel Button

--- src/Controller/AddTeacherController.java
+++ src/Controller/AddTeacherController.java
@@ -136,6 +136,7 @@ public class AddTeacherController implements Initializable {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+        
+        cancelBTN.setOnAction(EventHandle.teacherAddCancelBTNEvent());
     }
-
 }
