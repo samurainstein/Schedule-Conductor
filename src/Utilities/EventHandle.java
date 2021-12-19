9377164a02d0cb8217f12b80cede7fef11caeaff
samurainstein <eric.matelyan@gmail.com>
2021-12-19 11:16:32 -0500

    Create event handler for save button on add appointments screen

--- src/Utilities/EventHandle.java
+++ src/Utilities/EventHandle.java
@@ -18,6 +18,8 @@ import Model.InstrumentTeacher;
 import com.mysql.cj.util.StringUtils;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -867,7 +869,7 @@ public abstract class EventHandle {
         };
         return eventHandler;
     }
-    
+
     public static EventHandler<ActionEvent> appointmentCancelBTN() {
 
         EventHandler<ActionEvent> eventHandler = new EventHandler<ActionEvent>() {
@@ -887,15 +889,15 @@ public abstract class EventHandle {
     }
 
     public static EventHandler<ActionEvent> appointmentAddClearBTN(
-            TextField titleTF, 
-            TextArea descriptionTA, 
-            ComboBox<String> locationCB, 
-            DatePicker dateDP, 
-            ComboBox<LocalTime> timeCB, 
-            ComboBox<String> lengthCB, 
+            TextField titleTF,
+            TextArea descriptionTA,
+            ComboBox<String> locationCB,
+            DatePicker dateDP,
+            ComboBox<LocalTime> timeCB,
+            ComboBox<String> lengthCB,
             ComboBox<InstrumentTeacher> teacherCB,
             ComboBox<InstrumentStudent> studentCB
-            ) throws SQLException {
+    ) throws SQLException {
 
         EventHandler<ActionEvent> eventHandler = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
@@ -908,66 +910,88 @@ public abstract class EventHandle {
                 lengthCB.getSelectionModel().clearSelection();
                 teacherCB.getSelectionModel().clearSelection();
                 studentCB.getSelectionModel().clearSelection();
-                
+
             }
         };
         return eventHandler;
     }
 
-//    public static EventHandler<ActionEvent> appointmentAddSaveBTN(
-//            TextField nameTF,
-//            ComboBox<Country> countryCB,
-//            ComboBox<Division> divisionCB,
-//            TextField postalTF,
-//            TextField addressTF,
-//            TextField phoneTF,
-//            TextField instrumentTF,
-//            ToggleGroup onlineTGL,
-//            ToggleGroup inPersonTGL
-//    ) throws SQLException {
-//
-//        EventHandler<ActionEvent> eventHandler = new EventHandler<ActionEvent>() {
-//            public void handle(ActionEvent event) {
-//
-//                String name = nameTF.getText();
-//                String postal = postalTF.getText();
-//                String address = addressTF.getText();
-//                String phone = phoneTF.getText();
-//                String instrument = instrumentTF.getText();
-//                RadioButton onlineRadio = (RadioButton) onlineTGL.getSelectedToggle();
-//                char onlineRadioChar = onlineRadio.getText().charAt(0);
-//                RadioButton inPersonRadio = (RadioButton) inPersonTGL.getSelectedToggle();
-//                char inPersonRadioChar = inPersonRadio.getText().charAt(0);
-//
-//                if (StringUtils.isEmptyOrWhitespaceOnly(name)
-//                        || StringUtils.isEmptyOrWhitespaceOnly(postal)
-//                        || StringUtils.isEmptyOrWhitespaceOnly(address)
-//                        || StringUtils.isEmptyOrWhitespaceOnly(phone)
-//                        || StringUtils.isEmptyOrWhitespaceOnly(instrument)) {
-//                    Alerts.invalidFields();
-//                    return;
-//                }
-//
-//                try {
-//                    String country = countryCB.getSelectionModel().getSelectedItem().getCountryName();
-//                    String division = divisionCB.getSelectionModel().getSelectedItem().getDivisionName();
-//                    InstrumentStudentDAO.insertStudent(name, country, division, postal, address, phone,
-//                            instrument, onlineRadioChar, inPersonRadioChar);
-//                    try {
-//                        root = FXMLLoader.load(getClass().getResource("/View/Students.fxml"));
-//                    } catch (IOException ex) {
-//                        ex.printStackTrace();
-//                    }
-//                    pageTitle = PageLoader.getStudentsTitle();
-//                    stage = (Stage) ((Node) event.getTarget()).getScene().getWindow();
-//                    PageLoader.pageLoad(stage, root, pageTitle);
-//                } catch (NullPointerException ex) {
-//                    Alerts.countryOrDivisionNullAlert();
-//                }
-//
-//            }
-//        };
-//        return eventHandler;
-//    }
+    public static EventHandler<ActionEvent> appointmentAddSaveBTN(
+            TextField titleTF,
+            TextArea descriptionTA,
+            ComboBox<String> locationCB,
+            DatePicker dateDP,
+            ComboBox<LocalTime> timeCB,
+            ComboBox<String> lengthCB,
+            ComboBox<InstrumentTeacher> teacherCB,
+            ComboBox<InstrumentStudent> studentCB
+    ) throws SQLException {
+
+        EventHandler<ActionEvent> eventHandler = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+
+                String title = titleTF.getText();
+                String description = descriptionTA.getText();
+                String location = locationCB.getSelectionModel().getSelectedItem();
+                LocalDate date = dateDP.getValue();
+                LocalTime startTime = timeCB.getValue();
+                LocalTime endTime = timeCB.getValue();
+                String length = lengthCB.getSelectionModel().getSelectedItem();
+                InstrumentTeacher teacher = teacherCB.getSelectionModel().getSelectedItem();
+                InstrumentStudent student = studentCB.getSelectionModel().getSelectedItem();
+                
+                if (StringUtils.isEmptyOrWhitespaceOnly(title)
+                    || StringUtils.isEmptyOrWhitespaceOnly(description)) {
+                    Alerts.invalidFields();
+                    return;
+                }
+                
+                try {
+                    if (length.matches("30 Minutes")) {
+                        endTime = startTime.plusMinutes(30);
+                    } else if (length.matches("1 Hour")) {
+                        endTime = startTime.plusHours(1);
+                    } else if (length.matches("1 Hour 30 Minutes")) {
+                        endTime = startTime.plusMinutes(30);
+                        endTime = startTime.plusHours(1);
+                    } else if (length.matches("2 Hours")) {
+                        endTime = startTime.plusHours(2);
+                    }
+                    String teacherName = teacher.getName();
+                    int teacherId = teacher.getId();
+                    String studentName = student.getName();
+                    int studentId = student.getId();
+                    AppointmentDAO.selectAppointments();
+                    Boolean teacherOverlap = Data.checkTeacherOverlap(teacherId, startTime, endTime, date);
+                    Boolean studentOverlap = Data.checkStudentOverlap(studentId, startTime, endTime, date);
+                    if (!teacherOverlap && !studentOverlap) {
+                        LocalDateTime start = LocalDateTime.of(date, startTime);
+                        LocalDateTime end = LocalDateTime.of(date, endTime);
+                        AppointmentDAO.insertAppointment(title, description, location, start, end, teacherName, teacherId, studentName, studentId);
+
+                        Parent root;
+                        try {
+                            root = FXMLLoader.load(getClass().getResource("/View/Appointments.fxml"));
+                            stage = (Stage) ((Node) event.getTarget()).getScene().getWindow();
+                            String pageTitle = PageLoader.getAppointmentsTitle();
+                            PageLoader.pageLoad(stage, root, pageTitle);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                        
+                    } else {
+                        Alerts.appointmentOverlap();
+                    }
+
+                } catch (SQLException exception) {
+                    exception.printStackTrace();
+                } catch (NullPointerException exception) {
+                    Alerts.invalidFields();
+                }
+
+            }
+        };
+        return eventHandler;
+    }
 
 }
